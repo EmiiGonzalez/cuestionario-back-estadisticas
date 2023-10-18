ddd40786b83c2c31192143313926c754c3316c28
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2023-10-18 10:42:09 -0300

    Se soluciono un bug que cortaba los reintentos de actualizar la bd

--- src/index.js
+++ src/index.js
@@ -7,25 +7,18 @@ import { saveData } from "./services/saveDatos.js";
 
 dotenv.config({ path: ".env" });
 const port = process.env.PORT;
+const minutos = Number(process.env.MINUTOS);
 
 const actualizar = async () => {
   try {
     const url = process.env.URL;
-    const minutos = Number(process.env.MINUTOS);
-
     const resp = await consumirApi(url)
     const datos = getFrecuencias(resp);
     const frecuencias = tablaFrecuencias(datos);
-    saveData(frecuencias);
-
-    setTimeout( () => {
-      actualizar();
-      console.log("Se actualizo la base de datos");
-    } , minutos * 60 * 1000);
-  
+    await saveData(frecuencias);
+    console.log("Se actualizo la base de datos");
   } catch (error) {
-    console.log("Ocurrio un error al actualizar la base de datos, se reintentara en 30 minutos");
-    console.log(error);
+    console.log(`Ocurrio un error al actualizar la base de datos, se reintentara en ${minutos} minutos`);
   } 
 };
 
@@ -35,6 +28,11 @@ async function main() {
       console.log(`Servidor corriendo en el puerto ${port}`);
     });
     actualizar();
+
+    setTimeout( () => {
+      actualizar();
+    } , minutos * 60 * 1000);
+
   } catch (error) {
     console.error(error.message);
   }
